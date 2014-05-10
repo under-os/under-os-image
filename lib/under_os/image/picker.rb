8269e401f3091b048b0e992d01bc28d99a943ab5
Nikolay Nemshilov <nemshilov@gmail.com>
2014-05-10 10:56:41 +1000

    Adding the picked images preview and hidding the status-bar

--- lib/under_os/image/picker.rb
+++ lib/under_os/image/picker.rb
@@ -5,8 +5,9 @@
 class UnderOs::Image::Picker
   def initialize(options={})
     @animated   = options.delete(:animated) || true
-    @_          = UIImagePickerController.alloc.init
+    @_          = NoStatusBarPickerController.alloc.init
     @_.delegate = self
+    @_.view.backgroundColor = UnderOs::App.history.current_page.view._.backgroundColor
   end
 
   def take(&block)
@@ -19,6 +20,7 @@ class UnderOs::Image::Picker
   end
 
   def pick(&block)
+    @preview && @preview.remove
     @_.setSourceType(UIImagePickerControllerSourceTypePhotoLibrary)
     start(&block)
   end
@@ -29,8 +31,60 @@ class UnderOs::Image::Picker
     @page.presentViewController @_, animated: @animated, completion: nil
   end
 
-  def imagePickerController(picker, didFinishPickingImage:image, editingInfo:info)
+  def imagePickerController(picker, didFinishPickingMediaWithInfo:info)
+    image = UnderOs::Image.new(info[UIImagePickerControllerOriginalImage])
+
+    if @_.sourceType == UIImagePickerControllerSourceTypePhotoLibrary
+      preview image, picker.view
+    else
+      finish image # image from camera
+    end
+  end
+
+  def finish(image)
     @page.dismissModalViewControllerAnimated(@animated)
-    @block.call(UnderOs::Image.new(image))
+    @block.call(image)
+  end
+
+  def preview(image, main_page)
+    @preview ||= UnderOs::UI::View.new.tap do |view|
+      view.size = UnderOs::Screen.size
+      view.style.background = UnderOs::App.history.current_page.view.style.background
+
+      @preview_image = UnderOs::UI::Image.new.tap do |preview_image|
+        preview_image.insertTo(view)
+        preview_image.style = {width: 320, height: 426, top: 70, left: 0}
+      end
+
+      UnderOs::UI::Label.new(text: "Preview").tap do |title|
+        title.insertTo view
+        title.style = {width: '100%', height: 50, textAlign: :center}
+      end
+
+      UnderOs::UI::Button.new(text: "Use").tap do |button|
+        button.insertTo view
+        button.on(:tap) { finish(image) }
+        button.style = {right: 20, bottom: 10}
+      end
+
+      UnderOs::UI::Button.new(text: "Cancel").tap do |button|
+        button.insertTo view
+        button.on(:tap) { @preview.remove }
+        button.style = {left: 20, bottom: 10}
+      end
+    end
+
+    @preview_image.src = image
+    main_page.addSubview @preview._
+  end
+
+  class NoStatusBarPickerController < UIImagePickerController
+    def prefersStatusBarHidden
+      true
+    end
+
+    def childViewControllerForStatusBarHidden
+      nil
+    end
   end
 end
